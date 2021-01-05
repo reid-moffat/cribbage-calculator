3e76a88e34fb33cc451d3979617a25dc719be3d7
Reid Moffat <reid.moffat9@gmail.com>
2021-01-05 13:59:24 -0500

    added getAveragePoints

--- cribbage-calculator/src/main/UserInterface.java
+++ cribbage-calculator/src/main/UserInterface.java
@@ -3,6 +3,7 @@
  */
 package main;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Scanner;
@@ -46,12 +47,14 @@ final class UserInterface {
 		System.out.println("Created by Reid Moffat");
 
 		System.out.println("\nHow many players (2-4)?");
-		String numPlayers = in.next();
+		String numPlayers = in.nextLine();
 		while (!numPlayers.equals("2") && !numPlayers.equals("3") && !numPlayers.equals("4")) {
 			System.out.println("Invalid input. Try again:");
-			numPlayers = in.next();
+			numPlayers = in.nextLine();
 		}
 		int numCards = numPlayers.equals("2") ? 6 : 5;
+		System.out.println(numPlayers + " players: " + numCards + " cards to start");
+		this.hand = new String[numCards];
 
 		System.out.println("\nEach card must be its value (from 1 to 13) plus the suit");
 		System.out.println("Examples:");
@@ -61,11 +64,29 @@ final class UserInterface {
 		System.out.println("'13H': King of hearts");
 		System.out.println("Enter each of the " + numCards + " cards in your hand one by one below:\n");
 
-		for (int i = 1; i < numCards + 1; i++) {
-			String card = in.nextLine();
-			System.out.println("Card " + i + ": " + checkValidCard(card));
+		boolean noDuplicates = false;
+		while (!noDuplicates) {
+			for (int i = 0; i < numCards; i++) {
+				String card = in.nextLine();
+				String cardValue = checkValidCard(card);
+				while (cardValue == null) {
+					System.out.println("Invalid card, try again");
+					card = in.nextLine();
+					cardValue = checkValidCard(card);
+				}
+				hand[i] = card.toUpperCase();
+				System.out.println("Card " + (i + 1) + ": " + cardValue + "\n");
+			}
+			
+			noDuplicates = new HashSet<String>(Arrays.asList(hand)).size() == numCards;
+			
+			if (!noDuplicates) {
+				System.out.println("Duplicate card in hand, input cards again:\n");
+			}
 		}
-		
+
+		System.out.println(this.getAveragePoints());
+
 		in.close();
 	}
 
@@ -73,10 +94,10 @@ final class UserInterface {
 		if (card.length() < 2 || card.length() > 3) {
 			return null;
 		}
-		
+
 		try {
 			char suit;
-			card.toUpperCase();
+			card = card.toUpperCase();
 			if (card.length() == 2) {
 				if (card.charAt(0) < '1' || (int) card.charAt(0) > '9') {
 					return null;
@@ -94,23 +115,57 @@ final class UserInterface {
 		} catch (Exception e) {
 			return null;
 		}
-		
+
 		return valueToCard(card);
 	}
-	
+
 	private static String valueToCard(String card) {
-		final String[] ranks = {"Ace", "Two", "Three", "Four", "Five", "Six", "Seven",
-						  "Eight", "Nine", "Ten", "Jack", "Queen", "King"};
+		final String[] ranks = { "Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack",
+				"Queen", "King" };
 		final HashMap<Character, String> suits = new HashMap<Character, String>();
 		suits.put('C', "clubs");
 		suits.put('D', "diamonds");
 		suits.put('H', "hearts");
 		suits.put('S', "spades");
-		
+
 		int value = card.length() == 3 ? 10 + card.charAt(1) - '0' : card.charAt(0) - '0';
 		String rank = ranks[value - 1];
 		String suit = suits.get(card.charAt(card.length() - 1));
 		return rank + " of " + suit;
+	}
+
+	private String getAveragePoints() {
+		StringBuilder sb = new StringBuilder();
+		String[] handCopy;
+		
+		if (this.hand.length == 6) {
+			;
+		} else {
+			sb.append("Average points for each drop combination:");
+			for (int i = 0; i < hand.length; i++) {
+				double averagePoints = 0;
+				String droppedCard = this.hand[i];
+				for (String starterCard : this.cardPile) {
+					if (!this.containsCard(starterCard)) {
+						handCopy = Arrays.copyOf(hand, hand.length);
+						handCopy[i] = starterCard;
+						averagePoints += Calculators.totalPoints(handCopy);
+					}
+				}
+				averagePoints = Math.round(100*(averagePoints / 47)) / 100;
+				sb.append("\n" + valueToCard(droppedCard) + ": " + averagePoints);
+			}
+		}
+		return sb.toString();
+	}
+	
+	private boolean containsCard(String card) {
+		for (String s : this.hand) {
+			if (s.equals(card)) {
+				return true;
+			}
+		}
+		return false;
 	}
 
 	public static void main(String[] args) {
