2d2607ab8dd0c22ac8144f6bd9587c00f91d30b1
Reid Moffat <reid.moffat9@gmail.com>
2021-01-09 23:31:45 -0500

    Completed Suit enum

--- cribbage-calculator/src/main/Suit.java
+++ cribbage-calculator/src/main/Suit.java
@@ -1,5 +1,35 @@
 package main;
 
+/**
+ * The four card suits in a standard deck of 52 playing cards, ordered from
+ * lowest to highest value using alphabetical order:
+ * 
+ * <ul>
+ * 	<li>{@code CLUBS}
+ * 	<li>{@code DIAMONDS}
+ * 	<li>{@code HEARTS}
+ * 	<li>{@code SPADES}
+ * <ul>
+ * 
+ */
 enum Suit {
+	/**
+	 * The suit clubs
+	 */
+	CLUBS,
 
+	/**
+	 * The suit diamonds
+	 */
+	DIAMONDS,
+
+	/**
+	 * The suit hearts
+	 */
+	HEARTS,
+
+	/**
+	 * The suit spades
+	 */
+	SPADES
 }
