6c8408a5868e676419f75ded6f8cee54c487da38
Reid Moffat <reid.moffat9@gmail.com>
2021-01-09 22:31:34 -0500

    Added Card class and ranks/suit enums

---
+++ cribbage-calculator/src/main/Card.java
@@ -0,0 +1,19 @@
+package main;
+
+public final class Card implements Comparable<Card> {
+
+	public Card() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public int compareTo(Card o) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+}
---
+++ cribbage-calculator/src/main/Rank.java
@@ -0,0 +1,28 @@
+package main;
+
+enum Rank {
+	/**
+	 * The thirteen ranks of cards in a standard 52-card French deck. This
+	 * enumeration orders the ace as the highest rank;
+	 * the ranks in order from "smallest" rank to "greatest" rank is:
+	 * 
+	 * <ul>
+	 * <li>TWO
+	 * <li><code>THREE</code>
+	 * <li><code>FOUR</code>
+	 * <li><code>FIVE</code>
+	 * <li><code>SIX</code>
+	 * <li><code>SEVEN</code>
+	 * <li><code>EIGHT</code>
+	 * <li><code>NINE</code>
+	 * <li><code>TEN</code>
+	 * <li><code>JACK</code>
+	 * <li><code>QUEEN</code>
+	 * <li><code>KING</code>
+	 * <li><code>ACE</code>
+	 * </ul>
+	 *
+	 */
+	ONE
+
+}
