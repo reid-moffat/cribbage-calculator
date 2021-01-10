65abb89d924b883962ad7936b087fe8219a2f26c
Reid Moffat <reid.moffat9@gmail.com>
2021-01-10 15:39:01 -0500

    Finished Suit enum

--- cribbage-calculator/src/card/Suit.java
+++ cribbage-calculator/src/card/Suit.java
@@ -4,10 +4,10 @@ package card;
  * The four suits in a standard deck of 52 playing cards:
  * 
  * <ul>
- * 	<li><code>CLUBS</code></li>
- * 	<li><code>DIAMONDS</code></li>
- * 	<li><code>HEARTS</code></li>
- * 	<li><code>SPADES</code></li>
+ * <li><code>CLUBS</code></li>
+ * <li><code>DIAMONDS</code></li>
+ * <li><code>HEARTS</code></li>
+ * <li><code>SPADES</code></li>
  * </ul>
  * 
  */
