b712a150c1acff5600360391e8a116cabae7cc68
Reid Moffat <reid.moffat9@gmail.com>
2021-01-26 19:33:35 -0500

    Create package-info.java

---
+++ src/main/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Contains a user interface and static calculator method used to get user input
+ * and caclualte score from a given hand respectively
+ * 
+ * @author Reid Moffat
+ */
+package main;
