e30216a6082a26ee803431624aae11909b9825cf
Reid Moffat <reid.moffat9@gmail.com>
2021-02-01 18:07:23 -0500

    Updated package-info.java
    CribbageHand is no longer a static class

--- src/main/package-info.java
+++ src/main/package-info.java
@@ -1,6 +1,6 @@
 /**
- * Contains a user interface and static calculator method used to get user input
- * and caclualte score from a given hand respectively
+ * Contains a user interface and cribbage hand class used to get user input and
+ * caclualte score from a given hand respectively
  * 
  * @author Reid Moffat
  */
