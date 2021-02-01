8f6d0c870ddf5d259469f92ed8189f11824bb637
Reid Moffat <reid.moffat9@gmail.com>
2021-02-01 17:32:16 -0500

    Generalized Suit
    Added suit ordering

--- src/card/Suit.java
+++ src/card/Suit.java
@@ -1,14 +1,14 @@
 package card;
 
 /**
- * The four suits in a standard deck of 52 playing cards:
+ * The four suits in a standard deck of 52 playing cards ordered alphabetically:
  * 
- * <ul>
+ * <ol>
  * <li><code>CLUBS</code></li>
  * <li><code>DIAMONDS</code></li>
  * <li><code>HEARTS</code></li>
  * <li><code>SPADES</code></li>
- * </ul>
+ * </ol>
  * 
  */
 public enum Suit {
@@ -30,6 +30,28 @@ public enum Suit {
 	/**
 	 * The suit spades
 	 */
-	SPADES
+	SPADES;
+
+	/**
+	 * Returns the rank of this {@code Suit}
+	 * 
+	 * <P>
+	 * Ranks are alphabetical as follows:
+	 * 
+	 * <ol>
+	 * <li><code>SPADES</code></li>
+	 * <li><code>HEARTS</code></li>
+	 * <li><code>DIAMONDS</code></li>
+	 * <li><code>CLUBS</code></li>
+	 * </ol>
+	 * 
+	 * Note: 'Rank' has nothing to do with the card's rank (ACE, KING, THREE, SEVEN,
+	 * etc.), this is only for games in which {@code Suits} have different values
+	 * 
+	 * @return the rank of this {@code Suit}
+	 */
+	int getSuitRank() {
+		return 4 - this.ordinal();
+	}
 
 }
