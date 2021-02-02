f2ecff80ef07194843af4e66c1065baa8ca8c342
Reid Moffat <reid.moffat9@gmail.com>
2021-02-01 20:48:43 -0500

    Cleaned up UserInterface
    Increased conciseness of methods and improved documentation

--- src/main/UserInterface.java
+++ src/main/UserInterface.java
@@ -14,8 +14,8 @@ import card.Suit;
  * 
  * <p>
  * Used to get user input from the console for a cribbage hand, and use the
- * class {@code Calculators} to determine the optimal strategies for dropping
- * cards
+ * class {@code CribbageCombinations} to determine the optimal strategies for
+ * dropping cards
  * 
  * @author Reid Moffat
  */
@@ -38,10 +38,10 @@ final class UserInterface {
 	/**
 	 * Used to get user input from the console
 	 */
-	private final Scanner in;
+	private final Scanner input;
 
 	/**
-	 * A set of valid card ranks used to check if a user input is valid
+	 * A list of valid card ranks used to check if a user input is valid
 	 * 
 	 * <p>
 	 * IMPORTANT: 10 is represented by 'T', although the user must input '10' for a
@@ -67,10 +67,10 @@ final class UserInterface {
 			Arrays.asList('1', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K'));
 
 	/**
-	 * A set of valid card suits that the user can input
+	 * A list of valid card suits that the user can input
 	 * 
 	 * <p>
-	 * These are not part of the suits in card.Suit
+	 * These are not part of the suits in Card.Suit
 	 */
 	private static final ArrayList<Character> VALID_SUITS = new ArrayList<Character>(Arrays.asList('C', 'D', 'H', 'S'));
 
@@ -105,7 +105,7 @@ final class UserInterface {
 	 */
 	public UserInterface() {
 		this.dealthHand = new HashSet<Card>();
-		this.in = new Scanner(System.in);
+		this.input = new Scanner(System.in);
 	}
 
 	/**
@@ -151,43 +151,49 @@ final class UserInterface {
 	 * @return the number of starting cards for the given number of players
 	 */
 	private int getNumCards() {
+		/* Prompts the user to enter the number of players */
 		System.out.println(UserInterface.ENTER_PLAYERS);
 
-		String numPlayers = in.nextLine();
-		while (!numPlayers.equals("2") && !numPlayers.equals("3") && !numPlayers.equals("4")) {
+		/* Loops until a valid number of players is inputted */
+		String numPlayers = input.nextLine();
+		while (!(numPlayers.equals("2") || numPlayers.equals("3") || numPlayers.equals("4"))) {
 			System.out.println("Invalid input. Try again:");
-			numPlayers = in.nextLine();
+			numPlayers = input.nextLine();
 		}
 
+		/* Returns the number of cards dealt to each player */
 		return numPlayers.equals("2") ? 6 : 5;
 	}
 
 	/**
-	 * Prompts the user to enter the valid playing cards in their hand and stores
-	 * their values
+	 * Prompts the user to enter the playing cards in their hand and stores their
+	 * values
 	 * 
 	 * @param numCards the number of cards to be inputed
 	 */
 	private void getCards(int numCards) {
+		/* Prompts the user to enter their cards with a guide to valid inputs */
 		System.out.println(numCards + " cards to start");
 		System.out.println(UserInterface.ENTER_CARDS);
 
-		for (int i = 0; i < numCards; i++) {
-			Card card = checkValidCard(in.nextLine());
-			while (card == null || containsCard(card)) {
+		/* Gets and stores each valid card the user inputs */
+		for (int i = 1; i <= numCards; i++) {
+			Card card = checkValidCard(input.nextLine());
+			while (card == null || !notInHand(card)) {
 				System.out.println("Invalid or duplicate card, input again:\n");
-				card = checkValidCard(in.nextLine());
+				card = checkValidCard(input.nextLine());
 			}
 			this.dealthHand.add(card);
-			System.out.println("Card " + (i + 1) + ": " + card.toString() + "\n");
+			System.out.println("Card " + i + ": " + card.toString() + "\n");
 		}
 
-		this.in.close();
+		/* Close the scanner to prevent resource leak */
+		this.input.close();
 	}
 
 	/**
-	 * Calculates the average points obtained from a cribbage for each combination
-	 * of cards to be dropped and prints out the value
+	 * Calculates the average cribbage points obtained for each combination of cards
+	 * to be dropped and prints out the value
 	 * 
 	 * <p>
 	 * The average number of points takes into account the number of points gained
@@ -195,40 +201,54 @@ final class UserInterface {
 	 */
 	private void printAveragePoints() {
 		StringBuilder sb = new StringBuilder();
-		CribbageHand hand = new CribbageHand(this.dealthHand);
+		CribbageHand hand = new CribbageHand(new HashSet<Card>(this.dealthHand));
+
+		/*
+		 * The number of possible starter cards (a double value so dividing with it
+		 * won't floor the result to an int)
+		 */
+		final double unknownCards = 52 - hand.size();
 
 		/* With 6 cards, 2 must be dropped */
 		if (hand.size() == 6) {
 			sb.append("Average points for each drop combination:");
 			for (Card[] combination : subset2(hand.getCards())) {
-				double averagePoints = 0;
+				/* The two cards to be dropped */
 				hand.remove(combination[0]);
 				hand.remove(combination[1]);
-				for (Card starterCard : cardPile) {
-					if (!this.containsCard(starterCard) && !starterCard.equals(combination[0])
-							&& !starterCard.equals(combination[1])) {
-						averagePoints += hand.totalPoints(starterCard);
-					}
-				}
+
+				/* Calculate the total number of points (all starters) for this combination */
+				int totalPoints = cardPile.stream().filter(this::notInHand).mapToInt(hand::totalPoints).sum();
+
+				/* The combination and its average number of points to 2 decimals */
 				sb.append("\n" + combination[0].toString() + " and " + combination[1].toString() + ": "
-						+ Math.round(100 * (averagePoints / 46)) / 100.0);
+						+ Math.round(100 * (totalPoints / unknownCards)) / 100.0);
+
+				/* Put the dropped cards back in the hand */
 				hand.add(combination[0]);
 				hand.add(combination[1]);
 			}
-		} else { /* With 5 cards, only one needs to be dropped */
+
+			/* With 5 cards, only one needs to be dropped */
+		} else {
 			sb.append("Average points for each card dropped:");
 			for (Card droppedCard : hand.getCards()) {
-				double averagePoints = 0;
+				/* The card to be dropped */
 				hand.remove(droppedCard);
-				for (Card starterCard : cardPile) {
-					if (!this.containsCard(starterCard) && !starterCard.equals(droppedCard)) {
-						averagePoints += hand.totalPoints(starterCard);
-					}
-				}
-				sb.append("\n" + droppedCard.toString() + ": " + Math.round(100 * (averagePoints / 47)) / 100.0);
+
+				/* Calculate the total number of points (all starters) for this combination */
+				int totalPoints = cardPile.stream().filter(this::notInHand).mapToInt(hand::totalPoints).sum();
+
+				/* The combination and its average number of points to 2 decimals */
+				sb.append(
+						"\n" + droppedCard.toString() + ": " + Math.round(100 * (totalPoints / unknownCards)) / 100.0);
+
+				/* Put the dropped card back in the hand */
 				hand.add(droppedCard);
 			}
 		}
+
+		/* Prints out the average points for each drop combination */
 		System.out.println(sb.toString());
 	}
 
@@ -275,10 +295,10 @@ final class UserInterface {
 	 * card
 	 * 
 	 * @param card the card to check
-	 * @return true if the card is in the player's hand, false otherwise
+	 * @return true if the card is not in the player's hand, false otherwise
 	 */
-	private boolean containsCard(Card card) {
-		return this.dealthHand.contains(card);
+	private boolean notInHand(Card card) {
+		return !this.dealthHand.contains(card);
 	}
 
 	/**
@@ -302,8 +322,7 @@ final class UserInterface {
 	}
 
 	public static void main(String[] args) {
-		UserInterface UI = new UserInterface();
-		UI.run();
+		new UserInterface().run();
 	}
 
 }
